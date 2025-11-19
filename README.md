8fadae8e4fd78f7dd5ee60dd75f4133f633e6c3c
olivierbabasse <34944436+olivierbabasse@users.noreply.github.com>
2025-11-19 17:23:51 +0100

    naive implementation and simple optimizations

---
+++ README.md
@@ -0,0 +1,15 @@
+Using datafiles from https://huggingface.co/datasets/nietras/1brc.data
+
+Ubuntu 22.04 / i7-9700K CPU @ 3.60GHz / 32 GB RAM
+
+cat data/measurements-1000000000.txt > /dev/null : 1.233s
+
+naive implementation, default release build options : 96.503s
+naive implementation, codegen-units, lto, panic, target native, debug info : 78.692s
+using Vec of u8 instead of Strings and parsing as i32 : 60.295s
+names as 32-u8 array and custom hashing : 38.124s
+using FxHashMap instead of HashMap : 31.770s
+mmap input file instead of read_to_end() into Vec : 25.954s
+using memchr() instead of position() : 22.198s
+using references instead of copying names from buffer : 20.279s
+madvise, small line split optimizations : 17.773s
---
+++ src/main.rs
@@ -0,0 +1,154 @@
+use core::f32;
+use memchr::memchr;
+use memmap2::{Advice, Mmap};
+use rustc_hash::FxHashMap;
+use std::hash::Hash;
+use std::{
+    env::args,
+    fs::File,
+    io::{self, BufWriter, Write},
+    process::Command,
+    time::Instant,
+};
+
+#[derive(PartialEq, Eq, PartialOrd, Ord)]
+struct Name<'a>(&'a [u8]);
+
+impl<'a> Hash for Name<'a> {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        let ptr = self.0.as_ptr() as *const u32;
+        unsafe { ptr.read_unaligned() }.hash(state);
+    }
+}
+
+impl<'a> From<&Name<'a>> for String {
+    fn from(value: &Name) -> Self {
+        let raw = std::str::from_utf8(&value.0).unwrap();
+        raw.trim_end_matches('\0').to_string()
+    }
+}
+
+struct Entry {
+    count: u32,
+    min: i32,
+    max: i32,
+    sum: i32,
+}
+
+impl Entry {
+    fn new(value: i32) -> Self {
+        Self {
+            count: 1,
+            min: value,
+            max: value,
+            sum: value,
+        }
+    }
+
+    fn add_value(&mut self, value: i32) {
+        self.count += 1;
+        self.sum += value;
+        self.min = self.min.min(value);
+        self.max = self.max.max(value);
+    }
+
+    fn min(&self) -> f32 {
+        self.min as f32 / 10.0
+    }
+    fn max(&self) -> f32 {
+        self.max as f32 / 10.0
+    }
+    fn average(&self) -> f32 {
+        (self.sum as f32 / self.count as f32) / 10.0
+    }
+}
+
+fn parse_value_internal(text: &[u8]) -> i32 {
+    if text[1] == b'.' {
+        (text[0] - b'0') as i32 * 10 + (text[2] - b'0') as i32
+    } else {
+        (text[0] - b'0') as i32 * 100 + (text[1] - b'0') as i32 * 10 + (text[3] - b'0') as i32
+    }
+}
+fn parse_value(text: &[u8]) -> i32 {
+    if text[0] == b'-' {
+        -parse_value_internal(&text[1..])
+    } else {
+        parse_value_internal(text)
+    }
+}
+
+fn read_line(mut data: &[u8]) -> (&[u8], &[u8], &[u8]) {
+    let name: &[u8];
+    let value: &[u8];
+    (name, data) = data.split_at(memchr(b';', &data[3..]).unwrap() + 3);
+    data = &data[1..];
+    (value, data) = data.split_at(memchr(b'\n', &data[3..]).unwrap() + 3);
+    data = &data[1..];
+    (data, name, value)
+}
+
+fn main() -> io::Result<()> {
+    let base = args()
+        .nth(1)
+        .unwrap_or("data/measurements-1000000000".to_string());
+    let filename = format!("{base}.txt");
+    let output = format!("{base}.out");
+
+    let start_time = Instant::now();
+
+    let file = File::open(filename)?;
+    let data = unsafe { Mmap::map(&file)? };
+    data.advise(Advice::Sequential)?;
+
+    let mut entries = FxHashMap::default();
+
+    let mut buf = &*data;
+    while buf.len() >= 32 {
+        let name: &[u8];
+        let value: &[u8];
+        (buf, name, value) = read_line(buf);
+
+        let value = parse_value(value);
+        entries
+            .entry(Name(name))
+            .and_modify(|e: &mut Entry| e.add_value(value))
+            .or_insert(Entry::new(value));
+    }
+
+    let mut entries = entries.into_iter().collect::<Vec<_>>();
+    entries.sort_unstable_by_key(|(name, _)| name.0);
+
+    eprintln!("elapsed time : {}s", start_time.elapsed().as_secs_f64());
+
+    let file = File::create("./results.txt")?;
+    let mut writer = BufWriter::new(file);
+    write!(writer, "{{")?;
+    write!(
+        writer,
+        "{}",
+        entries
+            .iter()
+            .map(|(name, e)| {
+                let n: String = name.into();
+                format!("{}={:.1}/{:.1}/{:.1}", n, e.min(), e.average(), e.max())
+            })
+            .collect::<Vec<_>>()
+            .join(", ")
+    )?;
+    writeln!(writer, "}}")?;
+    writer.flush()?;
+    drop(writer);
+
+    match Command::new("cmp")
+        .arg(output)
+        .arg("./results.txt")
+        .status()?
+        .code()
+    {
+        Some(0) => eprintln!("results match expected output"),
+        _ => eprintln!("*** ERROR : results do not match expected output"),
+    }
+
+    Ok(())
+}
